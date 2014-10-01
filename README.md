312e5e27a37b61cc7530a13b0de6d831815782cb
limitusus <limit.usus@gmail.com>
2014-10-01 20:00:26 +0900

    init via milla

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Data::Rmap::XS - Blah blah blah
+
+# SYNOPSIS
+
+    use Data::Rmap::XS;
+
+# DESCRIPTION
+
+Data::Rmap::XS is
+
+# AUTHOR
+
+Tomoya KABE <limitusus@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2014- Tomoya KABE
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Data/Rmap/XS.pm
@@ -0,0 +1,39 @@
+package Data::Rmap::XS;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Data::Rmap::XS - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Data::Rmap::XS;
+
+=head1 DESCRIPTION
+
+Data::Rmap::XS is
+
+=head1 AUTHOR
+
+Tomoya KABE E<lt>limitusus@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Tomoya KABE
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
